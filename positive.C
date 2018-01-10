877ce15225f74f376628c1fce8f25fa8ba4e1dff
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-01-10 23:03:56 +1200

    positive
    positive

--- positive.C
+++ positive.C
@@ -1,21 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int num;
 printf("Enter the value:");
 scanf("%d",&num);
 if(num>0)
-{
 printf("The value is positive");
-}
 else if(num<0)
-{
 printf("The value is negative");
-}
 else
-{
-printf("The value is zero);
-}
-getch();
+printf("The value is zero");
 }
